e758fa2224d5443cda82203677d29147655cb03f
Tom <linecontroltran@gmail.com>
2024-01-11 22:38:34 -0500

    Version 1

---
+++ demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWorldController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "hello-world";
+    }
+}
